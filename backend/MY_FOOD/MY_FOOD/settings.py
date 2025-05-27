5b6208aef1d1355a793a5954d1f77b0472ef4b4d
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-05-27 13:28:28 +0600

    refresh_token blacklisted is created for logout

--- backend/MY_FOOD/MY_FOOD/settings.py
+++ backend/MY_FOOD/MY_FOOD/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'User',
 ]
 
@@ -65,6 +66,8 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'BLACKLIST_AFTER_ROTATION': True,  
+    'ROTATE_REFRESH_TOKENS': True, 
 }
 
 
--- backend/MY_FOOD/User/views.py
+++ backend/MY_FOOD/User/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import CustomUser
 from .serializers import CustomUserSerializers
 # Create your views here.
@@ -9,3 +13,17 @@ class CustomUserModelView(CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializers
     permission_classes = [AllowAny]
+
+
+class LogoutView(APIView):
+    permission_classes = []
+    
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh']
+            token = RefreshToken(refresh_token)
+            # print(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
