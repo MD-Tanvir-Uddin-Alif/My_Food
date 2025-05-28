3bd701c58127dc3c72060479cf344f030ff276e6
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-05-28 18:51:01 +0600

    Food and category model created

--- backend/MY_FOOD/Food/admin.py
+++ backend/MY_FOOD/Food/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import CategoryModel, FoodModel
 # Register your models here.
+
+@admin.register(CategoryModel)
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(FoodModel)
+class FoodModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category', 'created_at', 'updated_at')
+    search_fields = ('name', 'category',)
--- backend/MY_FOOD/Food/models.py
+++ backend/MY_FOOD/Food/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CategoryModel(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class FoodModel(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField(blank=True,null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(CategoryModel, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to='food_images/', blank=True, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
