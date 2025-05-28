09b1d33c14914b881cc9fc10297b85bb8cc62fa4
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-05-28 19:06:57 +0600

    custom permision class and category serializer and view created

--- backend/MY_FOOD/Food/views.py
+++ backend/MY_FOOD/Food/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .models import CategoryModel
+from .permissions import OnlyAdminJob
+from .serializers import CategorySerializers
 # Create your views here.
+
+class CategoryView(ModelViewSet):
+    queryset = CategoryModel.objects.all()
+    serializer_class = CategorySerializers
+    permission_classes = [OnlyAdminJob]
