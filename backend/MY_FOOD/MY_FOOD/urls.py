a2d8feb467630d863c1def5c4e3c9330b268f61b
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-05-28 19:53:24 +0600

    category apis created

--- backend/MY_FOOD/MY_FOOD/urls.py
+++ backend/MY_FOOD/MY_FOOD/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('User.urls')),
+    path('api/', include('Food.url')),
 ]
 
 if settings.DEBUG:
