0edafa7aba949a090dd46eaaa3ec8977eb923759
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-05-30 00:42:45 +0600

    Navbar created

--- frontend/My_Food/src/App.jsx
+++ frontend/My_Food/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './index.css'
+import NavBar from './assets/components/NavBar'
 
 function App() {
 
   return (
     <div>
+      <NavBar/>
       <p className='bg-amber-300'>hudai</p>
     </div>
   )
---
+++ frontend/My_Food/src/assets/components/NavBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className='bg-white/80 backdrop-blur sticky top-0 z-0 p-'>
+        <div className='mx-auto flex items-center justify-between px-8'>
+          <div className='px-8'>
+            <img className='h-16 w-16' src="" alt="" />
+          </div>
+
+          <div className='flex items-center space-x-8 px-8 md:space-x-8'>
+            <p className='font-medium text-sm transition text-black'>Home</p>
+            <p className='font-medium text-sm transition text-black'>Food</p>
+            <p className='font-medium text-sm transition text-black'>Extra</p>
+          </div>
+
+          <div className='flex justify-between space-x-8 md:space-x-8'>
+            <p>
+              <button className="rounded-md px-4 py-2 text-black text-sm font-medium hover:bg-red-500 hover:text-white">
+                Login
+              </button>
+            </p>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
