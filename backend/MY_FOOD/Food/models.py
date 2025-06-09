e82d0aa234a1875a2f7a55a1dfc7af32267c8c47
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-06-09 16:48:16 +0600

    image added in category model

--- backend/MY_FOOD/Food/models.py
+++ backend/MY_FOOD/Food/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class CategoryModel(models.Model):
     name = models.CharField(max_length=100)
+    food_image = models.ImageField(upload_to='category_images/', blank=True, null=True)
     
     def __str__(self):
         return self.name
