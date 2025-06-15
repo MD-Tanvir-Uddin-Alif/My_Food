d4cc5a5f457633d4d4047a91292530e318f9084e
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-06-15 21:26:07 +0600

    view and routing for food order implemented

--- backend/MY_FOOD/User/urls.py
+++ backend/MY_FOOD/User/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .import views
+
+
+router = DefaultRouter()
+router.register(r'orders', views.OrderViewModel, basename='order')
 
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
@@ -12,4 +17,5 @@ urlpatterns = [
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('profile/', views.CustomUserProfileView.as_view(), name='user_profile'),
     path('profile/update/', views.UpdateCustomUserProfile.as_view(), name='profile_update'),
+    path('', include(router.urls)),
 ]
--- backend/MY_FOOD/User/views.py
+++ backend/MY_FOOD/User/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import CreateAPIView, RetrieveAPIView, RetrieveUpdateAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from .models import CustomUser
-from .serializers import CustomUserSerializers, CustomUserProfileSerializers, CustomTokenObtailPairSerializer
+from .models import CustomUser, OrderModel
+from .serializers import CustomUserSerializers, CustomUserProfileSerializers, CustomTokenObtailPairSerializer, OrderSerializer
 # Create your views here.
 
 class CustomUserModelView(CreateAPIView):
@@ -50,3 +51,17 @@ class UpdateCustomUserProfile(RetrieveUpdateAPIView):
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtailPairSerializer
+
+
+class OrderViewModel(ModelViewSet):
+    queryset = OrderModel.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return OrderModel.objects.all()
+        return OrderModel.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
