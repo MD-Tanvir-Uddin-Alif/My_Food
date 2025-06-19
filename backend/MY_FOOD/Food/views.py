756dd0f06d61a0f8fe23290f3c890c27494e3b8b
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-06-19 19:52:48 +0600

    search food by category implemented

--- backend/MY_FOOD/Food/views.py
+++ backend/MY_FOOD/Food/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.decorators import action, permission_classes
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
 from .models import CategoryModel, FoodModel
@@ -23,6 +25,14 @@ class FoodView(ModelViewSet):
     # permission_classes = [OnlyAdminJob]
     
     def get_permissions(self):
-        if self.action in ['list', 'retrieve']:
+        if self.action in ['list', 'retrieve', 'by_category']:
             return [AllowAny()]
         return [OnlyAdminJob()]
+    
+    @action(detail=False, url_path='category/(?P<category_name>[^/.]+)', methods=['get'])
+    @permission_classes([AllowAny])
+    def by_category(self, request, category_name=None):
+        category = get_object_or_404(CategoryModel, name__iexact=category_name)
+        foods = FoodModel.objects.filter(category=category)
+        serializer = self.get_serializer(foods, many=True)
+        return Response(serializer.data)
