e9b8361efb0cbcde78c7d90e220082d00d636d82
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-09-17 15:15:49 +0600

    forentend domain linked path removed

--- backend/MY_FOOD/MY_FOOD/settings.py
+++ backend/MY_FOOD/MY_FOOD/settings.py
@@ -95,7 +95,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://my-food-gray.vercel.app/",
+    "https://my-food-gray.vercel.app",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
