88b439edc578929b39e7f8dda98e56c7d1882a8a
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-10-05 17:29:52 +0600

    time out problem fixed in server data loding:

--- frontend/My_Food/src/utils/axiosInstance.js
+++ frontend/My_Food/src/utils/axiosInstance.js
@@ -5,7 +5,7 @@ const baseURL = 'https://my-food-nju8.onrender.com';
 
 const axiosInstance = axios.create({
   baseURL,
-  timeout: 5000,
+  timeout: 80000,
   headers: {
     'Content-Type': 'application/json',
   },
